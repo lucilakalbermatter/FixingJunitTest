b7fadd5746cf95381201552466336e6ed3aea3b4
lucilakalbermatter <lucilakalbermatter@hotmail.com>
2021-12-10 14:29:17 +0100

    first commit

---
+++ src/test/java/correct/TestEmpListSize.java
@@ -0,0 +1,38 @@
+package correct;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import main.java.Employee;
+import main.java.EmployeeManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TestEmpListSize {
+
+	EmployeeManager EM = null;
+	
+	@BeforeEach
+
+
+	@Test
+	@DisplayName("Correct initial length of Employee List")
+
+
+	@Test
+	@DisplayName("Incorrect initial length of Employee List")
+
+
+	@Test
+	@DisplayName("2 is the Correct Edited Length of Employee List")
+
+
+	@Test
+	@DisplayName("4 is the Correct Edited length of Employee List")
+
+
+	@AfterEach
+
+}
